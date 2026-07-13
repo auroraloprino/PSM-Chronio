1ad864b9764073b687c3a692d55285867afda66f
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-07-13 16:52:10 +0200

    removed unused import

--- ui/src/main/java/com/unibo/android/ui/navigation/AppNavigation.kt
+++ ui/src/main/java/com/unibo/android/ui/navigation/AppNavigation.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.unibo.android.ui.board.BoardScreen
 import com.unibo.android.ui.calendar.CalendarScreen
 
 @Composable
