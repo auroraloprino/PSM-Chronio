eb037bbfae8c0ed4def174ae42f0a34dbd875a74
Aurora Loprino <aurora.loprino@studio.unibo.it>
2026-07-17 15:12:37 +0200

    uodate tag function

--- domain/src/main/java/com/unibo/android/domain/repositories/repositories/TagRepository.kt
+++ domain/src/main/java/com/unibo/android/domain/repositories/repositories/TagRepository.kt
@@ -6,5 +6,6 @@ interface TagRepository {
     suspend fun getAll(): List<TagModel>
     suspend fun getForEvent(eventId: Long): List<TagModel>
     suspend fun save(tag: TagModel): Long
+    suspend fun update(tag: TagModel)
     suspend fun delete(tag: TagModel)
 }
